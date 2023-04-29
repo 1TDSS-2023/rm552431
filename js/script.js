68767e9984c0dadc7e039fd83db829fdea4f4dd6
Matheus da Silva Ferreira <56132266+MatheusFerreira05@users.noreply.github.com>
2023-04-28 21:00:11 -0300

    Introdução ao Java Script 28/04

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict"
+//console.log("Olá Mundo!")
+
+// var nr1 = "10"
+// var nr2 = "5"
+
+// //Realizando operações com Strings:
+// //Função parseInt
+// console.log(parseInt(nr1)+parseInt(nr2))
+
+var nome1 = "Dubas"
+// let nome2 = "Sebartinho"
+// const nome3 = "Julia"
+
+if(nome1 != ""){
+    let nome1 = "Julia"
+}
+
+console.log(nome1)
